b20dbbbac714cdcdf06272c8656210812224c069
Lenar Lima <lenar.toledo@gmail.com>
2023-02-21 18:57:35 -0400

    Upload Sides Bar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,29 +2,29 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   firebaseConfig : {
-//     apiKey: "AIzaSyDreM4HZTZLjVLZ0u4CR-Un0tRu4LoezDE",
-//     authDomain: "mytunez-46394.firebaseapp.com",
-//     projectId: "mytunez-46394",
-//     storageBucket: "mytunez-46394.appspot.com",
-//     messagingSenderId: "903858077796",
-//     appId: "1:903858077796:web:5aa6246ab21c94bbd9df71"
-//   }
-// };
-
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDJDCpesXdHT1R_H9PJbwroPo8JMF7rLn8",
-    authDomain: "sandbox-my-tunez.firebaseapp.com",
-    projectId: "sandbox-my-tunez",
-    storageBucket: "sandbox-my-tunez.appspot.com",
-    messagingSenderId: "464466250068",
-    appId: "1:464466250068:web:1e166cd13d8ad9d7bfa0c1"
+  firebaseConfig : {
+    apiKey: "AIzaSyDreM4HZTZLjVLZ0u4CR-Un0tRu4LoezDE",
+    authDomain: "mytunez-46394.firebaseapp.com",
+    projectId: "mytunez-46394",
+    storageBucket: "mytunez-46394.appspot.com",
+    messagingSenderId: "903858077796",
+    appId: "1:903858077796:web:5aa6246ab21c94bbd9df71"
   }
 };
+
+// export const environment = {
+//   production: false,
+//   firebaseConfig: {
+//     apiKey: "AIzaSyDJDCpesXdHT1R_H9PJbwroPo8JMF7rLn8",
+//     authDomain: "sandbox-my-tunez.firebaseapp.com",
+//     projectId: "sandbox-my-tunez",
+//     storageBucket: "sandbox-my-tunez.appspot.com",
+//     messagingSenderId: "464466250068",
+//     appId: "1:464466250068:web:1e166cd13d8ad9d7bfa0c1"
+//   }
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
